2ed866f822b8e240e30015ce8c4a4739e358bc33
Yifan (Frank) Zhang <zsxqblz@gmail.com>
2019-04-14 17:51:26 +0000

    added startup script

---
+++ swinit.sh
@@ -0,0 +1,11 @@
+#! /bin/sh
+
+# Screen Setup
+export DISPLAY=:0.0
+#xauth add $DISPLAY . 4f13077b4bbf73ed6f972e8708b67b8f
+xset s off
+xset s noblank
+xset -dpms
+
+#Excecute Code
+/home/pi/Documents/Cooper-Steering-Wheel/stpi
